2b3ab5f6888bd19171c618e567bf04c43f6b306c
Juan Carlos Vega <profejuancarlosvega@gmail.com>
2023-03-09 10:13:38 -0300

    carpetas y estructura necesaria node

--- Mod 4_Unidad 3/app.js
+++ Mod 4_Unidad 3/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var galeriaRouter = require('./routes/galeria');
+var nosotrosRouter = require('./routes/nosotros');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/galeria',galeriaRouter);
+app.use('/nosotros',nosotrosRouter);
+app.use('/contacto',contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
