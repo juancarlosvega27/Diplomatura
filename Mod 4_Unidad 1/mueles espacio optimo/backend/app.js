3b8dc069791c5a300f5e7a785897295752c710be
Juan Carlos Vega <profejuancarlosvega@gmail.com>
2023-04-19 19:26:29 -0300

    Finzalización de bd con detalles a consultar

--- Mod 4_Unidad 1/mueles espacio optimo/backend/app.js
+++ Mod 4_Unidad 1/mueles espacio optimo/backend/app.js
@@ -3,10 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
+
+var session= require('express-session');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -19,10 +24,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'MO4247518MA5727643ni',
+  cookie:{maxAge:null},
+  resave:false,
+  saveUninitialized:true
+}))
+
+secured = async (req,res,next) =>{
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario){
+      next();
+      
+    }else{
+      res.redirect('/admin/login');
+    }
+  }catch(error){
+    console.log(error);
+  }
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
