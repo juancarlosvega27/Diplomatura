66fdc082706cd8ad5dcf9482b7f4f285f009c8b4
Juan Carlos Vega <profejuancarlosvega@gmail.com>
2023-05-10 18:04:32 -0300

    ultimos cambiós para la presentación final

--- Mod 4_Unidad 1/mueles espacio optimo/frontend/src/pages/NovedadesPage.js
+++ Mod 4_Unidad 1/mueles espacio optimo/frontend/src/pages/NovedadesPage.js
@@ -34,36 +34,3 @@ const NovedadesPage = (props) => {
 } 
 
 export default NovedadesPage;
-
-
-
-
-
-
-
-
-  /*   <main className="mainnovedades">
-        <div className="novedad">
-            <h2>Novedad 1</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolorem delectus molestiae sint
-                eligendi nesciunt vitae dolorum perspiciatis dicta, quas magnam quia accusamus? Repudiandae veniam
-                libero dolorem qui. Nisi, odit.
-            </h3>
-        </div>
-
-        <div className="novedad">
-            <h2>Novedad 2</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolorem delectus molestiae sint
-                eligendi nesciunt vitae dolorum perspiciatis dicta, quas magnam quia accusamus? Repudiandae veniam
-                libero dolorem qui. Nisi, odit.
-            </h3>
-        </div>
-        <div className="novedad">
-            <h2>Novedad 3</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolorem delectus molestiae sint
-                eligendi nesciunt vitae dolorum perspiciatis dicta, quas magnam quia accusamus? Repudiandae veniam
-                libero dolorem qui. Nisi, odit.
-            </h3>
-        </div>
-
-    </main> */
